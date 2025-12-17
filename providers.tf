00bbbe72ad9865401a1af6ec0621989493a5a20e
alitn <boubaker.alii@gmail.com>
2025-12-17 20:23:02 +0100

    update bucket name

--- providers.tf
+++ providers.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
+
 #Defining multiple providers using "alias" parameter
 provider "aws" {
   profile = var.profile
